f059dff3b89a3aacdd18c7f8061479d4cfb10e5b
John <john@k77.ru>
2010-01-30 13:27:27 +0000

    M201

---
+++ src/rabnet/gui/forms/ToolTipForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace rabnet
+{
+    public partial class ToolTipForm : Form
+    {
+        private static ToolTipForm ttfrm = null;
+        public static ToolTipForm getForm()
+        {
+            if (ttfrm == null)
+                ttfrm = new ToolTipForm();
+            return ttfrm;
+        }
+
+        public ToolTipForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolTipForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ttfrm = null;
+        }
+    }
+}
