a8c5591016d0d7b07dc47089f5aa88286d858b40
John <john@k77.ru>
2010-03-28 23:36:17 +0000

    keycoder

---
+++ src/keycoder/MainForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace KeyCoder
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
