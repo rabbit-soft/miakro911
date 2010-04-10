3c57c26af2ac080daeea84c22999142e8c1782ab
John <john@k77.ru>
2010-04-10 11:01:57 +0000

    fixed #195

---
+++ src/rabdump/RestoreForm.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace rabdump
+{
+    partial class RestoreForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbDB = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(408, 481);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Восстановить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(327, 481);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Отмена";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(144, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Место резервирования";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "База Данных";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(144, 45);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(12, 72);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(493, 237);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 315);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Расширенный режим <<";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Дата";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Имя файла";
+            this.columnHeader2.Width = 291;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.tbFile);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.tbDB);
+            this.groupBox1.Controls.Add(this.tbPassword);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tbUser);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbHost);
+            this.groupBox1.Location = new System.Drawing.Point(12, 344);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(493, 128);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Расширенный режим";
+            // 
+            // tbHost
+            // 
+            this.tbHost.Location = new System.Drawing.Point(77, 19);
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(126, 20);
+            this.tbHost.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Хост";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(218, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Пользователь";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(304, 19);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(100, 20);
+            this.tbUser.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(253, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Пароль";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(304, 45);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPassword.TabIndex = 5;
+            // 
+            // tbDB
+            // 
+            this.tbDB.Location = new System.Drawing.Point(77, 49);
+            this.tbDB.Name = "tbDB";
+            this.tbDB.Size = new System.Drawing.Size(126, 20);
+            this.tbDB.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Имя БД";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 95);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Файл копии";
+            // 
+            // tbFile
+            // 
+            this.tbFile.Location = new System.Drawing.Point(81, 92);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.Size = new System.Drawing.Size(373, 20);
+            this.tbFile.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(460, 90);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(27, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Резервные копии (*.dump;*.7z)|*.dump;*.7z|Сжатые резервные копии (*.7z)|*.7z|Дамп" +
+                "ы БД(*.dump)|*.dump|Все файлы (*.*)|*.*";
+            // 
+            // RestoreForm
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(517, 516);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "RestoreForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Восстановить резервную копию";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbHost;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox tbFile;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbDB;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ src/rabdump/RestoreForm.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace rabdump
+{
+    public partial class RestoreForm : Form
+    {
+        private ArchiveJob jj = null;
+        public RestoreForm()
+        {
+            InitializeComponent();
+            setMode(true);
+            foreach (ArchiveJob j in Options.get().Jobs)
+                comboBox1.Items.Add(j.Name);
+            comboBox1.SelectedIndex = 0;
+        }
+
+        public RestoreForm(String place):this()
+        {
+            for (int i = 0; i < comboBox1.Items.Count; i++)
+            {
+                if ((string)comboBox1.Items[i] == place)
+                    comboBox1.SelectedIndex = i;
+            }
+        }
+
+        public void setMode(bool small)
+        {
+            Height = (small ? 410 : 550);
+            button3.Text="Расширенный режим "+(small?">>":"<<");
+            groupBox1.Visible = !small;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           setMode(Height==550);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            listView1.Items.Clear();
+            foreach (ArchiveJob j in Options.get().Jobs)
+                if (j.Name == comboBox1.Text)
+                {
+                    jj = j;
+                    if (j.DB == DataBase.AllDataBases)
+                    {
+                        foreach (DataBase db in Options.get().Databases)
+                            comboBox2.Items.Add(db.Name);
+                    }
+                    else
+                        comboBox2.Items.Add(j.DB.Name);
+                }
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void fillList(ArchiveJob j, String db)
+        {
+            listView1.Items.Clear();
+            DirectoryInfo di = new DirectoryInfo(j.BackupPath);
+            foreach (FileInfo fi in di.GetFiles())
+            {
+                String[] nm = Path.GetFileName(fi.FullName).Split('_');
+                if (nm.Length == ArchiveJobThread.SPLIT_NAMES && nm[0]==j.Name && nm[1]==db)
+                {
+                    string[] hms = new string[3] { nm[5].Substring(0, 2), nm[5].Substring(2, 2), nm[5].Substring(4, 2) };
+                    DateTime dtm = new DateTime(int.Parse(nm[2]), int.Parse(nm[3]), int.Parse(nm[4]), int.Parse(hms[0]), int.Parse(hms[1]), int.Parse(hms[2]));
+                    int idx=0;
+                    for (int i = 0; i < listView1.Items.Count;i++ )
+                    {
+                        if (DateTime.Parse(listView1.Items[i].SubItems[0].Text) > dtm)
+                            idx++;
+                    }
+                    ListViewItem li=listView1.Items.Insert(idx,dtm.ToShortDateString()+" "+dtm.ToLongTimeString());
+                    li.SubItems.Add(Path.GetFileName(fi.FullName));
+                }
+            }
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataBase db = jj.DB;
+            if (db == DataBase.AllDataBases)
+                foreach (DataBase d in Options.get().Databases)
+                    if (d.Name == comboBox2.Text) db = d;
+            tbHost.Text = db.Host;
+            tbDB.Text = db.DBName;
+            tbUser.Text = db.User;
+            tbPassword.Text = db.Password;
+            fillList(jj, comboBox2.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbFile.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count!=1) return;
+            tbFile.Text=jj.BackupPath+"\\"+listView1.SelectedItems[0].SubItems[1].Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ArchiveJobThread.undumpdb(tbHost.Text, tbDB.Text, tbUser.Text, tbPassword.Text, tbFile.Text);
+                MessageBox.Show("Восстановление завершено");
+                Close();
+            }
+            catch (Exception ex)
+            {
+                DialogResult = DialogResult.None;
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
