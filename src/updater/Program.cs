beb64e8498722145394ab083fa6e601a05f796b3
X-plora <the.xplora@gmail.com>
2010-09-10 12:25:56 +0000

    Bug #294: Fixed bug #294

--- src/updater/Program.cs
+++ src/updater/Program.cs
@@ -13,28 +13,35 @@ namespace updater
         [STAThread]
         static void Main(string[] args)
         {
-            bool batch = false;
-            if (args.Length>0)
-                batch = args[0] == "batch";
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            String fl = Path.GetDirectoryName(Application.ExecutablePath) + "\\rabdump.exe.config";
-            bool update = File.Exists(fl);
-            //update = true;
-            int res=0;
-            if (update)
-            {
-                UpdateForm uf = new UpdateForm(fl,batch);
-                Application.Run(uf);
-                res = uf.result;
-            }
-            else
-            {
-                InstallForm ifr=new InstallForm(fl,batch);
-                Application.Run(ifr);
-                res = ifr.result;
-            }
-            Environment.ExitCode = res;
+//            try
+//            {
+                bool batch = false;
+                if (args.Length > 0)
+                    batch = args[0] == "batch";
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                String fl = Path.GetDirectoryName(Application.ExecutablePath) + "\\rabdump.exe.config";
+                bool update = File.Exists(fl);
+                //update = true;
+                int res = 0;
+                if (update)
+                {
+                    UpdateForm uf = new UpdateForm(fl, batch);
+                    Application.Run(uf);
+                    res = uf.result;
+                }
+                else
+                {
+                    InstallForm ifr = new InstallForm(fl, batch);
+                    Application.Run(ifr);
+                    res = ifr.result;
+                }
+                Environment.ExitCode = res;
+//            }
+//            catch (Exception e)
+//            {
+//                MessageBox.Show(e.Message + e.StackTrace);
+//            }
         }
     }
 }
