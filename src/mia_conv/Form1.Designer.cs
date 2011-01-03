7a559fa3ca0637009a749f9e7be15b770f2b7929
X-plora <the.xplora@gmail.com>
2011-01-03 18:28:38 +0000

    small fixes

--- src/mia_conv/Form1.Designer.cs
+++ src/mia_conv/Form1.Designer.cs
@@ -420,6 +420,7 @@ namespace mia_conv
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Импорт фермы";
             this.Activated += new System.EventHandler(this.Form1_Activated);
             this.groupBox1.ResumeLayout(false);
