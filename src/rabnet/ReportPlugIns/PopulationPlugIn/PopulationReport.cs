3847ee1faf315654c749485cbac496e48026226a
Gambit Kikos <gam6itko@gmail.com>
2013-04-10 13:48:31 +0000

    tatishevo report fix

--- src/rabnet/ReportPlugIns/PopulationPlugIn/PopulationReport.cs
+++ src/rabnet/ReportPlugIns/PopulationPlugIn/PopulationReport.cs
@@ -44,19 +44,19 @@ namespace rabnet
 
         private string getSQL(DateTime dt)
         {
-            return String.Format(@"create temporary table aaa as select
+            return String.Format(@"create temporary table aaa as SELECT
   CONCAT('{2:s}',IF(Month(NOW())='{4:d}' AND Year(NOW())={3:d},' (now)','')) month,
   '{3:d}' year,
-  (select Coalesce(sum(r_group),0) from rabbits where r_born<'{0:s}') + (select Coalesce(sum(r_group),0) from dead where r_born<'{0:s}' and d_date>='{0:s}') bcount,
-  (select Coalesce(sum(f_children+f_added),0) from fucks where f_state='okrol' and f_end_date>='{0:s}' and f_end_date<'{1:s}') born,
-  (select Coalesce(sum(t_count),0) from income where t_date>= '{0:s}' and t_date<'{1:s}') buy,
-#(select Coalesce(sum(r_group),0) from dead where d_reason>2 and d_date>='{0:s}' and d_date<'{1:s}') gonetotal,
-  (select Coalesce(sum(r_group),0) from dead where d_reason=3 and d_date>='{0:s}' and d_date<'{1:s}' ) killed,
-  (select Coalesce(sum(r_group),0) from dead where d_reason=4 and d_date>='{0:s}' and d_date<'{1:s}') sell,
-  (select Coalesce(sum(r_group),0) from dead where (d_reason=5 or d_reason=6) and d_date>='{0:s}' and d_date<'{1:s}' ) dying,
-  (select Coalesce(sum(r_group),0) from dead where d_reason>6 and d_date>='{0:s}' and d_date<'{1:s}' ) another,  
-  (select Coalesce(sum(r_group),0) from rabbits where r_born<'{1:s}') + (select Coalesce(sum(r_group),0) from dead where r_born<'{1:s}' and d_date>='{1:s}') ecount;
-select `month`, `year`, bcount, (born+buy) income,born, buy,(killed+dying+sell+another) gonetotal, killed, dying, sell, another, ecount from aaa;
+  (SELECT Coalesce(sum(r_group),0) FROM rabbits WHERE r_born<'{0:s}') + (SELECT Coalesce(sum(r_group),0) FROM dead WHERE r_born<'{0:s}' AND d_date>='{0:s}') bcount,
+  (SELECT Coalesce(sum(f_children+f_added),0) FROM fucks where f_state='okrol' AND f_end_date>='{0:s}' AND f_end_date<'{1:s}') born,
+  (SELECT Coalesce(sum(t_count),0) FROM import WHERE t_date>= '{0:s}' AND t_date<'{1:s}') buy,
+#(select Coalesce(sum(r_group),0) FROM dead WHERE d_reason>2 AND d_date>='{0:s}' AND d_date<'{1:s}') gonetotal,
+  (SELECT Coalesce(sum(r_group),0) FROM dead WHERE d_reason=3 AND d_date>='{0:s}' AND d_date<'{1:s}' ) killed,
+  (SELECT Coalesce(sum(r_group),0) FROM dead WHERE d_reason=4 AND d_date>='{0:s}' AND d_date<'{1:s}') sell,
+  (SELECT Coalesce(sum(r_group),0) FROM dead WHERE (d_reason=5 OR d_reason=6) AND d_date>='{0:s}' AND d_date<'{1:s}' ) dying,
+  (SELECT Coalesce(sum(r_group),0) FROM dead WHERE d_reason>6 AND d_date>='{0:s}' AND d_date<'{1:s}' ) another,  
+  (SELECT Coalesce(sum(r_group),0) FROM rabbits WHERE r_born<'{1:s}') + (SELECT Coalesce(sum(r_group),0) FROM dead WHERE r_born<'{1:s}' AND d_date>='{1:s}') ecount;
+SELECT `month`, `year`, bcount, (born+buy) income,born, buy,(killed+dying+sell+another) gonetotal, killed, dying, sell, another, ecount FROM aaa;
 drop temporary table aaa;",
                 dt.ToString("yyyy-MM-dd"), dt.AddMonths(1).ToString("yyyy-MM-dd"), toRusMonth(dt.Month), dt.Year,dt.Month);
         }
