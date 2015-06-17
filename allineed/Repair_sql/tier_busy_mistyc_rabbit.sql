abb2f9fd9548a1535ac83b91e4e29d610bcfa912
Gambit Kikos <gam6itko@gmail.com>
2015-06-17 14:10:15 +0000

    script

--- allineed/Repair_sql/tier_busy_mistyc_rabbit.sql
+++ allineed/Repair_sql/tier_busy_mistyc_rabbit.sql
@@ -1,5 +1,9 @@
-select t from (select t_busy1 t from tiers where t_busy1<>0 union
-	select t_busy2 t from tiers where t_busy2<>0 union
-	select t_busy3 t from tiers where t_busy3<>0 and t_busy3 is not null union
-	select t_busy4 t from tiers where t_busy4<>0 and t_busy4 is not null) bus 
-where t not in(select r_id from rabbits);
+## -- иногда происходит, что кролика списали, но он все еще сидит в клетке. От этого клетка нигде не фигурирует
+select t_id, rid, p
+from (
+	select t_id, t_busy1 rid, 1 AS p from tiers where t_busy1<>0 union
+	select t_id, t_busy2 rid, 2 AS p from tiers where t_busy2<>0 union
+	select t_id, t_busy3 rid, 3 AS p from tiers where t_busy3<>0 and t_busy3 is not null union
+	select t_id, t_busy4 rid, 4 AS p from tiers where t_busy4<>0 and t_busy4 is not null
+) bus 
+where rid not in(select r_id from rabbits);
