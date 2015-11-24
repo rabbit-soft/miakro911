764c29470060131c9ea8b95b3181b4fd12555dfa
Gambit Kikos <gam6itko@gmail.com>
2015-11-24 10:09:20 +0000

    FK

--- src/updater/sql/17.sql
+++ src/updater/sql/17.sql
@@ -43,11 +43,47 @@ ALTER TABLE  `rabbits` CHANGE  `r_father`  `r_father` INT( 10 ) UNSIGNED NULL DE
 UPDATE rabbits SET r_mother = NULL WHERE r_mother = 0;
 UPDATE rabbits SET r_father = NULL WHERE r_father = 0;
 
-ALTER TABLE  `rabbits` ADD FOREIGN KEY (  `r_parent` ) REFERENCES  `rabbits` (`r_id`) 
+ALTER TABLE  `rabbits` ADD FOREIGN KEY (`r_parent`) REFERENCES  `rabbits` (`r_id`) 
 	ON DELETE SET NULL 
 	ON UPDATE CASCADE;
 
+	
+ALTER TABLE  `rabbits` CHANGE  `r_farm`  `r_farm` INT( 10 ) UNSIGNED NULL DEFAULT NULL ;
+UPDATE rabbits SET r_farm = NULL WHERE r_farm =0;
 
+ALTER TABLE  `rabbits` ADD FOREIGN KEY (`r_farm`) REFERENCES  `minifarms` (`m_id`) 
+	ON DELETE SET NULL 
+	ON UPDATE CASCADE ;
+	
+ALTER TABLE  `rabbits` CHANGE  `r_tier`  `r_tier` INT( 10 ) UNSIGNED NULL DEFAULT NULL ;
+UPDATE rabbits SET r_tier=null WHERE r_tier=0;
+
+ALTER TABLE  `rabbits` ADD FOREIGN KEY (`r_tier`) REFERENCES  `tiers` (`t_id`) 
+	ON DELETE SET NULL 
+	ON UPDATE CASCADE ;
+	
+ALTER TABLE  `rabbits` ADD FOREIGN KEY (`r_breed`) REFERENCES  `breeds` (`b_id`) 
+	ON DELETE RESTRICT 
+	ON UPDATE CASCADE ;
+	
+ALTER TABLE  `rabbits` CHANGE  `r_name`  `r_name` INT( 10 ) UNSIGNED NULL DEFAULT NULL ;
+UPDATE rabbits SET r_name = NULL WHERE r_name =0;
+
+ALTER TABLE  `rabbits` ADD FOREIGN KEY (`r_name`) REFERENCES  `names` (`n_id`) 
+	ON DELETE RESTRICT 
+	ON UPDATE CASCADE ;
+	
+ALTER TABLE  `rabbits` CHANGE  `r_surname`  `r_surname` INT( 10 ) UNSIGNED NULL DEFAULT NULL ;
+UPDATE rabbits SET r_surname = NULL WHERE r_surname =0;
+ALTER TABLE  `rabbits` ADD FOREIGN KEY (`r_surname`) REFERENCES  `names` (`n_id`) 
+	ON DELETE RESTRICT 
+	ON UPDATE CASCADE ;
+	
+ALTER TABLE  `rabbits` CHANGE  `r_secname`  `r_secname` INT( 10 ) UNSIGNED NULL DEFAULT NULL ;
+UPDATE rabbits SET r_secname = NULL WHERE r_secname =0;
+ALTER TABLE  `rabbits` ADD FOREIGN KEY (`r_secname`) REFERENCES  `names` (`n_id`) 
+	ON DELETE RESTRICT 
+	ON UPDATE CASCADE ;
 
 
 UPDATE options SET o_value = '17' WHERE o_name = 'db' AND o_subname = 'version';
