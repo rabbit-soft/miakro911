f1f693688ed0b0e03c040bcb2596f20449e3f5bc
Gambit Kikos <gam6itko@gmail.com>
2016-05-17 13:46:39 +0000

    code format

--- src/rabdump/Forms/RestoreForm.cs
+++ src/rabdump/Forms/RestoreForm.cs
@@ -1,5 +1,5 @@
 ﻿#if DEBUG
-    //#define NOCATCH
+//#define NOCATCH
 #endif
 using System;
 using System.Threading;
@@ -31,12 +31,10 @@ namespace rabdump
         {
             InitializeComponent();
             SetMode(true);
-            foreach (ArchiveJob j in Options.Inst.Jobs)
-            {
+            foreach (ArchiveJob j in Options.Inst.Jobs) {
                 cbJobName.Items.Add(j.Name);
             }
-            if (cbJobName.Items.Count > 0)
-            {
+            if (cbJobName.Items.Count > 0) {
                 cbJobName.SelectedIndex = 0;
             }
             _wtFrm = new WaitForm();
@@ -44,10 +42,10 @@ namespace rabdump
 
         }
 
-        public RestoreForm(String place):this()
+        public RestoreForm(String place)
+            : this()
         {
-            for (int i = 0; i < cbJobName.Items.Count; i++)
-            {
+            for (int i = 0; i < cbJobName.Items.Count; i++) {
                 if ((string)cbJobName.Items[i] == place)
                     cbJobName.SelectedIndex = i;
             }
@@ -60,14 +58,14 @@ namespace rabdump
         public void SetMode(bool small)
         {
             Height = (small ? 410 : 550);
-            btExtMode.Text="Расширенный режим "+(small?">>":"<<");
+            btExtMode.Text = "Расширенный режим " + (small ? ">>" : "<<");
             groupBox1.Visible = !small;
             _smallMode = small;
         }
 
         private void btExtMode_Click(object sender, EventArgs e)
         {
-           SetMode(Height==550);
+            SetMode(Height == 550);
         }
 
         private void cbJobName_SelectedIndexChanged(object sender, EventArgs e)
@@ -76,8 +74,7 @@ namespace rabdump
             listView1.Items.Clear();
 
             foreach (ArchiveJob j in Options.Inst.Jobs)
-                if (j.Name == cbJobName.Text)
-                {
+                if (j.Name == cbJobName.Text) {
                     _jj = j;
                     //if (j.DataSrc == DataSource.AllDataBases) //TODO вернуть
                     //{
@@ -85,11 +82,11 @@ namespace rabdump
                     //        cbDataBase.Items.Add(db.Name);
                     //}
                     //else
-                        cbDataBase.Items.Add(j.DataSrc.Name);
+                    cbDataBase.Items.Add(j.DataSrc.Name);
                 }
-            if (cbDataBase.Items.Count!=0)
+            if (cbDataBase.Items.Count != 0)
                 cbDataBase.SelectedIndex = 0;
-            
+
         }
 
         /// <summary>
@@ -108,15 +105,12 @@ namespace rabdump
             {
 #endif
 #if PROTECTED ||DEBUG
-                try
-                {
-                    //sDump[] dmps = RabServWorker.ReqSender.ExecuteMethod(MethodName.GetDumpList).Value as sDump[];
-                    //servDumps = new List<sDump>(dmps);
-                }
-                catch (Exception exc)
-                {
-                    log.Warn(exc);
-                }
+            try {
+                //sDump[] dmps = RabServWorker.ReqSender.ExecuteMethod(MethodName.GetDumpList).Value as sDump[];
+                //servDumps = new List<sDump>(dmps);
+            } catch (Exception exc) {
+                log.Warn(exc);
+            }
 #endif
 #if PROTECTED               
             }
@@ -124,15 +118,12 @@ namespace rabdump
 
             int idx;
             DateTime dtm;
-            foreach (FileInfo fi in di.GetFiles())
-            {
-                if (Path.GetFileName(fi.FullName).StartsWith(searchName))
-                {
+            foreach (FileInfo fi in di.GetFiles()) {
+                if (Path.GetFileName(fi.FullName).StartsWith(searchName)) {
                     String[] nm = ArchiveJobThread.ParseDumpName(fi.Name);
                     dtm = DateTime.Parse(String.Format("{0}-{1}-{2} {3}:{4}:{5}", nm[2], nm[3], nm[4], nm[5].Substring(0, 2), nm[5].Substring(2, 2), nm[5].Substring(4, 2)));
                     idx = 0;
-                    for (int i = 0; i < listView1.Items.Count; i++)
-                    {
+                    for (int i = 0; i < listView1.Items.Count; i++) {
                         if (DateTime.Parse(listView1.Items[i].SubItems[0].Text) > dtm)
                             idx++;
                     }
@@ -142,8 +133,7 @@ namespace rabdump
                     ///Если такой дамп имеется на сервере, то окрашиваем строку в зеленый                    
                     if (servDumps != null && servDumps.Count > 0)
                         for (int i = 0; i < servDumps.Count; i++)
-                            if (servDumps[i].FileName == fi.Name)
-                            {
+                            if (servDumps[i].FileName == fi.Name) {
                                 li.ForeColor = System.Drawing.Color.Green;
                                 servDumps.RemoveAt(i);
                                 break;
@@ -152,17 +142,14 @@ namespace rabdump
                 }
             }
             ///Добавляем файлы которые имеются только на сервере
-            if (servDumps != null && servDumps.Count > 0 /*&& j.DataSrc != DataBase.AllDataBases*/)
-            {
-                foreach (sDump dmp in servDumps)
-                {
+            if (servDumps != null && servDumps.Count > 0 /*&& j.DataSrc != DataBase.AllDataBases*/) {
+                foreach (sDump dmp in servDumps) {
                     String[] nm = ArchiveJobThread.ParseDumpName(dmp.FileName);
                     dtm = DateTime.Parse(String.Format("{0}-{1}-{2} {3}:{4}:{5}", nm[2], nm[3], nm[4], nm[5].Substring(0, 2), nm[5].Substring(2, 2), nm[5].Substring(4, 2)));
                     idx = 0;
-                    for (int i = 0; i < listView1.Items.Count; i++)
-                    {
+                    for (int i = 0; i < listView1.Items.Count; i++) {
                         if (DateTime.Parse(listView1.Items[i].SubItems[0].Text) > dtm)
-                            idx++;                        
+                            idx++;
                     }
                     ListViewItem li = listView1.Items.Insert(idx, dtm.ToShortDateString() + " " + dtm.ToLongTimeString());
                     li.SubItems.Add(dmp.FileName);
@@ -172,7 +159,7 @@ namespace rabdump
         }
 
         private void cbDataBase_SelectedIndexChanged(object sender, EventArgs e)
-        {         
+        {
             DataSource db = _jj.DataSrc;
             //if (db == DataBase.AllDataBases)
             //    foreach (DataSource d in Options.Get().Databases)
@@ -181,22 +168,21 @@ namespace rabdump
             tbDB.Text = db.Params.DataBase;
             tbUser.Text = db.Params.User;
             tbPassword.Text = db.Params.Password;
-            tbFile_TextChanged(null,null);
+            tbFile_TextChanged(null, null);
             FillList(_jj, cbDataBase.Text);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
                 tbFile.Text = openFileDialog1.FileName;
             }
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (listView1.SelectedItems.Count!=1) return;
-            tbFile.Text=_jj.DumpPath+"\\"+listView1.SelectedItems[0].SubItems[1].Text;
+            if (listView1.SelectedItems.Count != 1) return;
+            tbFile.Text = _jj.DumpPath + "\\" + listView1.SelectedItems[0].SubItems[1].Text;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -206,21 +192,19 @@ namespace rabdump
 
         private void btRestore_Click(object sender, EventArgs e)
         {
-            if (_smallMode)
-            {
-                if (listView1.SelectedItems.Count == 0)
-                {
+            if (_smallMode) {
+                if (listView1.SelectedItems.Count == 0) {
                     MessageBox.Show("Выберите Резервную Копию для востановления");
                     return;
                 }
-                if (listView1.Items.Count!=0 && listView1.SelectedItems[0].Index != 0)
+                if (listView1.Items.Count != 0 && listView1.SelectedItems[0].Index != 0)
                     if (MessageBox.Show("Выбранная Резервная копия не является самой поздней. Продолжить?", "Внимание!", MessageBoxButtons.YesNo) == DialogResult.No)
                         return;
-            }           
+            }
             _thrRestore = new Thread(new ParameterizedThreadStart(RestoreThr));
             RestoreRarams p = new RestoreRarams();
 
-            p.Host=tbHost.Text;
+            p.Host = tbHost.Text;
             p.Db = tbDB.Text;
             p.User = tbUser.Text;
             p.Password = tbPassword.Text;
@@ -236,18 +220,18 @@ namespace rabdump
             _thrRestore.Start(p);
 
 
-//            try
-//            {
+            //            try
+            //            {
 
-//                ArchiveJobThread.UndumpDB(tbHost.Text, tbDB.Text, tbUser.Text, tbPassword.Text, tbFile.Text);
-//                MessageBox.Show("Восстановление завершено");
-//                Close();
-//            }
-//            catch (Exception ex)
-//            {
-//                DialogResult = DialogResult.None;
-//                MessageBox.Show(ex.Message);
-//            }
+            //                ArchiveJobThread.UndumpDB(tbHost.Text, tbDB.Text, tbUser.Text, tbPassword.Text, tbFile.Text);
+            //                MessageBox.Show("Восстановление завершено");
+            //                Close();
+            //            }
+            //            catch (Exception ex)
+            //            {
+            //                DialogResult = DialogResult.None;
+            //                MessageBox.Show(ex.Message);
+            //            }
         }
 
         public delegate void RestoreThrCbDelegate(bool success, Exception ex);
@@ -255,27 +239,21 @@ namespace rabdump
 
         private void RestoreThrCb(bool success, Exception ex)
         {
-            if (InvokeRequired)
-            {
+            if (InvokeRequired) {
                 RestoreThrCbDelegate d = RestoreThrCb;
                 Invoke(d, new object[] { success, ex });
                 //Invoke(d);
-            }
-            else
-            {     
+            } else {
                 _wtFrm.Hide();
-                if (success)
-                {
+                if (success) {
                     MessageBox.Show("Восстановление базы данных прошло успешно!", "Восстановление", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     DialogResult = DialogResult.OK;
                     Close();
-                } 
-                else
-                {
+                } else {
                     BringToFront();
-                    MessageBox.Show(ex.Message,"Ошибка при восстановлении",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    MessageBox.Show(ex.Message, "Ошибка при восстановлении", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     log.Error(ex.Message);
-//                    log.Error(ex.Message);
+                    //                    log.Error(ex.Message);
                     Enabled = true;
                     BringToFront();
                 }
@@ -286,42 +264,35 @@ namespace rabdump
         private void RestoreThr(object prms)
         {
 #if !NOCATCH
-            try
-            {
+            try {
 #endif
-                RestoreRarams p = (RestoreRarams) prms;
+                RestoreRarams p = (RestoreRarams)prms;
 
-    #if PROTECTED
+#if PROTECTED
                 if (GRD.Instance.GetFlag(GRD.FlagType.ServerDump))
                 {
-    #endif
-                    if (p.fromServer)
-                    {
-                        string filepath = RabServWorker.DownloadDump(Path.GetFileName(p.File));
-                        if (filepath == "")
-                        {
-                            RestoreThrCb(false, new ApplicationException("Проблема при скачки файла." + Environment.NewLine + "Востановление отменено"));
-                            return;
-                        }
-                        else
-                        {
-                            File.Move(filepath, p.File);
-                        }
+#endif
+                if (p.fromServer) {
+                    string filepath = RabServWorker.DownloadDump(Path.GetFileName(p.File));
+                    if (filepath == "") {
+                        RestoreThrCb(false, new ApplicationException("Проблема при скачки файла." + Environment.NewLine + "Востановление отменено"));
+                        return;
+                    } else {
+                        File.Move(filepath, p.File);
                     }
-    #if PROTECTED
                 }
-    #endif
+#if PROTECTED
+                }
+#endif
 
                 ArchiveJobThread.UndumpDB(p.Host, p.Db, p.User, p.Password, p.File);
 #if !NOCATCH
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 RestoreThrCb(false, ex);
                 return;
             }
 #endif
-                RestoreThrCb(true,null);
+            RestoreThrCb(true, null);
         }
 
         private void tbFile_TextChanged(object sender, EventArgs e)
