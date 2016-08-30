0974a82c995c3542afd5f9c73723cc9185daa161
Gambit <gam6itko@gmail.com>
2016-08-30 22:59:50 +0300

    ignore

--- allineed/allineed_install.cmd
+++ allineed/allineed_install.cmd
@@ -1,3 +1,2 @@
-copy ..\..\host_spec.include ..\..\host_spec.include.local
-copy .\build\host_spec.include.example ..\..\host_spec.include
+copy .\build\host_spec.include.example ..\host_spec.include
 copy .\build\*.cmd ..\.
